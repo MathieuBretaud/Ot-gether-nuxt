829ec88c6767551883aa072900e8f21d7f0bf341
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-14 16:28:01 +0200

    add images

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav-bar/>
+<!--    <AppHeader />-->
+    <slot />
+<!--    <AppFooter />-->
+  </div>
+</template>
+<script setup lang="ts">
+import NavBar from "~/layouts/navBar.vue";
+</script>
