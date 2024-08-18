678a1a2260c402ac472d88a11323cd2f24cab276
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-18 15:08:18 +0200

    responsive img landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,34 @@
 <template>
-  <main class="container mx-auto">
-    <!-- HERO SECTION -->
-    <section
-        class="flex items-center justify-between h-[100vh] pt-[5vh] gap-4"
-    >
-      <div class="flex flex-col space-y-3">
-        <h1 class="text-l lg:text-xl text-center text-navyBlue mb-6">
-          Des évenements pour partager de bons moments.
+  <UContainer>
+    <main>
+      <!-- HERO SECTION -->
+      <section
+          class="flex flex-col sm:flex-row sm:h-[100vh] items-center h-[90vh] pt-[5vh] gap-4"
+      >
+        <div class="flex flex-col space-y-3">
+          <h1 class="text-l lg:text-xl text-center text-navyBlue mb-6">
+            Des évenements pour partager de bons moments.
+          </h1>
+          <p class="text-lg opacity-80">
+            Quels que soient tes centres d'intérêts,
+            crée ou rejoins un événement sur Ot'gether.
+            Viens découvrir les événements proches de chez toi. C'est gratuit !
+          </p>
+        </div>
+
+        <NuxtImg src="/image-hero.jpg" sizes="100vw sm:50vw md:800px"/>
+      </section>
+
+      <!-- Tab section -->
+      <section>
+        <h1 class="text-4xl font-medium px-4">
+          All from a single source of truth
         </h1>
-        <p class="text-lg opacity-80">
-          Quels que soient tes centres d'intérêts,
-          crée ou rejoins un événement sur Ot'gether.
-          Viens découvrir les événements proches de chez toi. C'est gratuit !
-        </p>
-      </div>
 
-      <NuxtImg src="/image-hero.jpg" sizes="100vw sm:50vw md:800px"/>
-    </section>
+      </section>
 
-    <!-- Tab section -->
-    <section>
-      <h1 class="text-4xl font-medium px-4">
-        All from a single source of truth
-      </h1>
-
-    </section>
-
-  </main>
+    </main>
+  </UContainer>
 </template>
+<script setup lang="ts">
+</script>
