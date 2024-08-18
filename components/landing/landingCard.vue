4879c50fcff580e32f4033801cb62fa1140c98b2
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-18 15:50:24 +0200

    landingPage done

---
+++ components/landing/landingCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <UCard :ui="{ header: { background: 'bg-gradientBg'} }">
+    <template #header>
+      <h3 class="text-s mb-4">Apéro Party</h3>
+    </template>
+
+    <div class="text-body-m text-grey mb-6">
+      Découvrez l'art de l'apéritif. Savourez des cocktails exquis et des
+      amuse-bouches délicieux dans une ambiance conviviale et festive.
+    </div>
+  </UCard>
+</template>
+
