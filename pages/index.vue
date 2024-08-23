5438e26b81578739482264a2cf7abc419772c058
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-23 21:12:04 +0200

    fix nav

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <main>
       <!-- HERO SECTION -->
       <section
-          class="flex flex-col sm:flex-row sm:h-[100vh] items-center h-[90vh] pt-[5vh] gap-4"
+          class="flex flex-col sm:flex-row sm:h-[100vh] items-center h-[90vh] gap-4"
       >
         <div class="flex flex-col space-y-3">
           <h1 class="text-l lg:text-xl text-center text-navyBlue mb-6">
@@ -15,11 +15,10 @@
             Viens découvrir les événements proches de chez toi. C'est gratuit !
           </p>
         </div>
-
         <NuxtImg src="/image-hero.jpg" sizes="100vw sm:50vw md:800px"/>
       </section>
 
-      <section class="grid grid-cols-1 sm:grid-cols-3 gap-3 mb-4">
+      <section class="grid grid-cols-1 sm:grid-cols-3 gap-6 mb-7">
         <UCard class="bg-gradientOrangePink hover:bg-gradientOrangePinkHover">
           <h2 class="text-m text-white mb-4">Découvrez toutes les catégories d'évènements !</h2>
         </UCard>
