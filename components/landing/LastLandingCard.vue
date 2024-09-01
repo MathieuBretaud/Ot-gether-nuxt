824ba22e87e501209547e3441baa4c267ee7b255
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-01 20:38:46 +0200

    fix size image

---
+++ components/landing/LastLandingCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--  <UCard-->
+  <!--      :style="{ backgroundImage: `url(${event.category.image_url})`, backgroundSize: 'cover', backgroundPosition: 'center' }">-->
+  <UCard :ui="{ header: { padding: 0} }">
+    <template #header>
+      <NuxtImg :src="event.category.image_url" width="400" height="300" />
+
+    </template>
+    <h3 class="text-s mb-4 text-gray">{{ event.title }}</h3>
+    <div class="text-body-m text-gray mb-6">
+      {{ event.description }}
+    </div>
+    <template #footer>
+      <div class="text-body-m text-gray mb-6">
+        {{ event.category.name }}
+      </div>
+      <UButton>Button</UButton>
+    </template>
+  </UCard>
+</template>
+<script setup lang="ts">
+defineProps<{
+  event: any
+}>()
+</script>
+
