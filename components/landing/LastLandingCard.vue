447a21f72bf9905bf058be5b1fe21689f4a5d7b4
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-01 20:57:38 +0200

    fix size card footer

--- components/landing/LastLandingCard.vue
+++ components/landing/LastLandingCard.vue
@@ -3,20 +3,22 @@
   <!--      :style="{ backgroundImage: `url(${event.category.image_url})`, backgroundSize: 'cover', backgroundPosition: 'center' }">-->
   <UCard :ui="{ header: { padding: 0} }">
     <template #header>
-      <NuxtImg :src="event.category.image_url" width="400" height="300" />
-
+      <NuxtImg :src="event.category.image_url" width="400" height="300"/>
     </template>
-    <h3 class="text-s mb-4 text-gray">{{ event.title }}</h3>
-    <div class="text-body-m text-gray mb-6">
-      {{ event.description }}
-    </div>
-    <template #footer>
+
+    <div class="h-56">
+      <UBadge color="gray">{{ event.category.name }}</UBadge>
+      <h3 class="text-s mb-4 text-gray mt-6">{{ event.title }}</h3>
       <div class="text-body-m text-gray mb-6">
-        {{ event.category.name }}
+        {{ event.description }}
       </div>
+    </div>
+
+    <template #footer>
       <UButton>Button</UButton>
     </template>
   </UCard>
+
 </template>
 <script setup lang="ts">
 defineProps<{
