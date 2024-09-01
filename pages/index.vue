a86362dca499de6e09ebc00402488de76f1f6c4c
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-01 20:41:56 +0200

    landingPage lastLandingPage

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,15 @@
         <NuxtImg src="/image-hero.jpg" sizes="100vw sm:50vw md:800px"/>
       </section>
 
+      <section class="my-6">
+        <h2 class="text-l lg:text-l text-center text-navyBlue mb-6">Les derniers évènements</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-4 gap-6 mb-7">
+          <template v-for="event in data" :key="event.id">
+            <LastLandingCard :event="event"/>
+          </template>
+        </div>
+      </section>
+
       <section class="grid grid-cols-1 sm:grid-cols-3 gap-6 mb-7">
         <UCard class="bg-gradientOrangePink hover:bg-gradientOrangePinkHover">
           <h2 class="text-m text-white mb-4">Découvrez toutes les catégories d'évènements !</h2>
@@ -34,4 +43,15 @@
 
 </template>
 <script setup lang="ts">
+// onMounted(async () => {
+
+import LastLandingCard from "~/components/landing/LastLandingCard.vue";
+
+const {data, status, error, refresh, clear} = await useFetch('/api/events/last', {
+  method: 'GET',
+})
+
+// })
+
+
 </script>
