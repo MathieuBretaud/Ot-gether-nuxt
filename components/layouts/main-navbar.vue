15c3bf78fa35e51aa34fa2ef8f40164f5d928b2c
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-07 15:31:40 +0200

    page home

--- components/layouts/main-navbar.vue
+++ components/layouts/main-navbar.vue
@@ -12,8 +12,8 @@
         <NuxtLink to="/" class="flex items-center space-x-1">
           <NuxtImg src="/logo-otgether.png" class="h-12"/>
         </NuxtLink>
-        <NuxtLink to="/">
-          <UButton color="red" variant="solid" size="xl">Evénements</UButton>
+        <NuxtLink to="/home">
+          <UButton color="red" variant="solid" size="xl">Accueil</UButton>
         </NuxtLink>
 
         <div class="flex items-center space-x-2">
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="grid h-screen">
     <LayoutsMainNavbar />
     <slot />
     <LayoutsFooter />
