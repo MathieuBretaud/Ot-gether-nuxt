a49f9156f2becedf244cffcf96cd66f72c305f61
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-08 00:49:49 +0200

    pagination done

--- pages/home.vue
+++ pages/home.vue
@@ -1,33 +1,37 @@
 <script setup lang="ts">
-import LastLandingCard from "~/components/landing/LastLandingCard.vue";
+import LastLandingCard from "~/components/EventCard.vue";
 
-const page: any = ref(1)
-const items: any = ref([])
+const page: any = ref(1);
 
-
-const {data, status, error, refresh, clear} = await useFetch('/api/events/all', {
+const {data} = useFetch('/api/events/all', {
   method: 'GET',
-})
-items.value = data.value!.data;
-page.value = data.value!.current_page
-console.log(items.value)
+  params: {
+    page: page,
+    watch: [page],
+  },
+});
+
 </script>
 
 <template>
   <UContainer>
-    <section v-if="items" class="my-6">
-      <h2 class="text-l lg:text-l text-center text-navyBlue mb-6">Les derniers évènements</h2>
+    <section class="my-6">
+      <h2 class="text-l lg:text-l text-center text-navyBlue mb-6">
+        Les derniers évènements
+      </h2>
       <div class="grid grid-cols-1 sm:grid-cols-4 gap-6 mb-7">
-        <template v-for="event in items" :key="event.id">
-          <LastLandingCard :event="event"/>
+        <template v-if="data">
+          <LastLandingCard v-for="event in data.data" :key="event.id" :event="event"/>
         </template>
       </div>
     </section>
 
-<!--    <UPagination v-model="page" :page-count="5" :total="items.length"/>-->
+    <UPagination
+        v-model="page"
+        :page-count="data!.meta.per_page"
+        :total="data!.meta.total"
+    />
   </UContainer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
