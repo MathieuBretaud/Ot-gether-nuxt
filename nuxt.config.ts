ef764ec989fe8ff2a8502fd4a3e92d4e83d61f76
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-20 20:23:46 +0200

    add typescript check

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    typescript: {
+        typeCheck: true
+    },
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
     modules: [
-      "@nuxt/ui",
-      "@nuxt/image",
-      "@nuxtjs/tailwindcss",
-      "@nuxt/fonts",
-      "dayjs-nuxt",
+        "@nuxt/ui",
+        "@nuxt/image",
+        "@nuxtjs/tailwindcss",
+        "@nuxt/fonts",
+        "dayjs-nuxt",
     ],
     colorMode: {
         preference: 'light',
