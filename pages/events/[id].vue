c10fcd49325de08b4a9ec6b5ae94bcd4fa83b6ac
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-09-21 18:56:28 +0200

    wip page event detail

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import type {EventResponse, EventType} from "~/types";
+import {formatDate} from "~/helpers";
 
 const route = useRoute();
 const {data, pending, error} = useFetch<EventResponse>(`/api/events/${route.params.id}`);
@@ -10,11 +11,18 @@ const {data, pending, error} = useFetch<EventResponse>(`/api/events/${route.para
 
 <template>
   <UContainer>
-    <div v-if="data" class="flex mt-6">
-      <NuxtImg :src="data.data.image_url" width="400" height="300" fill/>
-      <div>
-        <h1>{{ data.data.title }}</h1>
+    <div v-if="data" class="my-6 flex justify-center items-center bg-gradientBg p-6 text-navyBlue">
+      <div class="flex flex-col">
+        <h1 class="text-m lg:text-l text-center">{{ data.data.title }}</h1>
         <p>{{ data.data.description }}</p>
+        <div class="flex flex-col ">
+          <strong>Participants {{ data.data.participants_count }} / {{ data.data.participant_max }}</strong>
+          <p>{{ data.data.category }}</p>
+          <p>Début de l'événement : {{ formatDate(data.data.start_date) }}</p>
+        </div>
+      </div>
+      <div class="ml-4">
+        <NuxtImg :src="data.data.image_url" width="400" height="300" fill/>
       </div>
     </div>
   </UContainer>
